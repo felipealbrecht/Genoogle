94561ebfee5b77de41adce9610f9628255a54755
albrecht <albrecht@c56685f5-d0d1-4d63-ba2d-6bec8b09bb16>
2007-12-27 08:04:07 +0000

    Forgot to commit in the changeset #17

---
+++ src/bio/pih/util/SymbolListWindowIteratorFactory.java
@@ -0,0 +1,51 @@
+package bio.pih.util;
+
+import org.biojava.bio.symbol.SymbolList;
+
+/**
+ * Factory for the {@link SymbolList} sliding windows
+ * 
+ * @author Albrecht
+ */
+public abstract class SymbolListWindowIteratorFactory {
+
+	private static SymbolListWindowIteratorFactory OVERLAPPED_SYMBOL_LIST_WINDOW_ITERATOR_FACTORY = new SymbolListWindowIteratorFactory.OverlappedSymbolListWindowIteratorFactory();
+	private static SymbolListWindowIteratorFactory NOT_OVERLAPPED_SYMBOL_LIST_WINDOW_ITERATOR_FACTORY = new SymbolListWindowIteratorFactory.NotOverlappedSymbolListWindowIteratorFactory();
+
+	/**
+	 * @param symbolList
+	 * @param windowLength
+	 * @return a new {@link SymbolListWindowIterator} related with this factory.
+	 */
+	public abstract SymbolListWindowIterator newSymbolListWindowIterator(SymbolList symbolList, int windowLength);
+
+	/**
+	 * @return a overlapped symbol list window iterator factory
+	 */
+	public static SymbolListWindowIteratorFactory getOverlappedFactory() {
+		return OVERLAPPED_SYMBOL_LIST_WINDOW_ITERATOR_FACTORY;
+	}
+
+	/**
+	 * @return a not overlapped symbol list window iterator factory
+	 */
+	public static SymbolListWindowIteratorFactory getNotOverlappedFactory() {
+		return NOT_OVERLAPPED_SYMBOL_LIST_WINDOW_ITERATOR_FACTORY;
+	}
+
+	private static class OverlappedSymbolListWindowIteratorFactory extends SymbolListWindowIteratorFactory {
+
+		@Override
+		public SymbolListWindowIterator newSymbolListWindowIterator(SymbolList symbolList, int windowLength) {
+			return new OverlappedSymbolListWindowIterator(symbolList, windowLength);
+		}
+	}
+
+	private static class NotOverlappedSymbolListWindowIteratorFactory extends SymbolListWindowIteratorFactory {
+
+		@Override
+		public SymbolListWindowIterator newSymbolListWindowIterator(SymbolList symbolList, int windowLength) {
+			return new NotOverlappedSymbolListWindowIterator(symbolList, windowLength);
+		}
+	}
+}
