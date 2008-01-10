3ceaf6d18b0415350368bf8d47734cf96be8f2cb
albrecht <albrecht@c56685f5-d0d1-4d63-ba2d-6bec8b09bb16>
2008-01-10 05:37:42 +0000

    Encoding and decoding sub-sequences. See more at task #1

--- src/bio/pih/index/SubSequencesArrayIndex.java
+++ src/bio/pih/index/SubSequencesArrayIndex.java
@@ -1,97 +1,112 @@
 package bio.pih.index;
 
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.DNATools;
 import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.symbol.FiniteAlphabet;
+import org.biojava.bio.symbol.IllegalSymbolException;
+import org.biojava.bio.symbol.Symbol;
 import org.biojava.bio.symbol.SymbolList;
 
+import bio.pih.seq.LightweightSymbolList;
 import bio.pih.util.SymbolListWindowIterator;
 import bio.pih.util.SymbolListWindowIteratorFactory;
 
 /**
  * @author albrecht
- *
+ * 
  */
 public class SubSequencesArrayIndex {
 
 	private static int maximumAlphabetBitsSize = 8;
 	private IndexBucket index[];
-	private final int alphabetSize;
 	private final int subSequenceLength;
+	private final int bitsByAlphabetSize;
+	private final FiniteAlphabet alphabet;
 	private final SymbolListWindowIteratorFactory symbolListWindowIteratorFactory;
-	private Class integerType;
-	private int bitsByAlphabet;
-	
-		
-	public SubSequencesArrayIndex(int subSequenceLength, int alphabetSize, SymbolListWindowIteratorFactory symbolListWindowIteratorFactory) throws ValueOutOfBoundsException {
-		assert(symbolListWindowIteratorFactory != null);
-		assert(subSequenceLength > 0);
-		assert(alphabetSize > 0);
-		
+//	private Class integerType;
+
+	public SubSequencesArrayIndex(int subSequenceLength, FiniteAlphabet alphabet, SymbolListWindowIteratorFactory symbolListWindowIteratorFactory) throws ValueOutOfBoundsException {
+		assert (symbolListWindowIteratorFactory != null);
+		assert (alphabet != null);
+		assert (subSequenceLength > 0);
+
 		this.subSequenceLength = subSequenceLength;
-		this.alphabetSize = alphabetSize;		
+		this.alphabet = alphabet;
 		this.symbolListWindowIteratorFactory = symbolListWindowIteratorFactory;
-		
-		int indexSize = subSequenceLength * bitsByAlphabetSize(alphabetSize);
-		this.integerType = getClassFromSize(indexSize);		
+
+		bitsByAlphabetSize = bitsByAlphabetSize(alphabet.size());
+		int indexSize = subSequenceLength * bitsByAlphabetSize;
+//		this.integerType = getClassFromSize(indexSize);
 		this.index = new IndexBucket[indexSize];
 	}
-		
-	/** 
-	 * @param alphabetSize must be equal or higher than 1 and equals or lower than 256 
+
+	/**
+	 * @param alphabetSize
+	 *            must be equal or higher than 1 and equals or lower than 256
 	 * @return
-	 * @throws ValueOutOfBoundsException is size is between from 1 to 64 
+	 * @throws ValueOutOfBoundsException
+	 *             is size is between from 1 to 64
 	 */
 	public static int bitsByAlphabetSize(int alphabetSize) throws ValueOutOfBoundsException {
 		if (alphabetSize <= 0) {
 			throw new ValueOutOfBoundsException("alphabetSize lower than zero.");
 		}
-		if(alphabetSize > (1 << maximumAlphabetBitsSize)) {
+		if (alphabetSize > (1 << maximumAlphabetBitsSize)) {
 			throw new ValueOutOfBoundsException("alphabetSize higher than " + (1 << maximumAlphabetBitsSize));
 		}
-		
-		int maxValue = alphabetSize-1;
+
+		int maxValue = alphabetSize - 1;
 		if (maxValue == 0) {
 			return 1;
 		}
-		
+
 		int bits = maximumAlphabetBitsSize;
 		while ((maxValue & (1 << bits)) == 0) {
 			bits--;
 		}
 		// the "one" that was used above must be added.
-		return bits+1;		
+		return bits + 1;
 	}
-	
+
+	/**
+	 * Add a sequence into index
+	 * @param sequence
+	 */
 	public void addSequence(Sequence sequence) {
 		if (sequence == null) {
-			throw new NullPointerException("sequence can not be null");
+			throw new NullPointerException("Sequence can not be null");
 		}
-		
+
 		SymbolListWindowIterator symbolListWindowIterator = symbolListWindowIteratorFactory.newSymbolListWindowIterator(sequence, this.subSequenceLength);
 
 		while (symbolListWindowIterator.hasNext()) {
 			SymbolList next = symbolListWindowIterator.next();
 			SubSequenceInfo subSequenceInfo = new SubSequenceInfo(sequence, next, symbolListWindowIterator.getActualPos(), this.subSequenceLength);
 			addSubSequence(next, subSequenceInfo);
-		}		
+		}
 	}
-	
+
 	/**
 	 * 
 	 * @param size
-	 * @return the integer Class that is need to store the value passed in size param.
+	 * @return the integer Class that is need to store the value passed in size
+	 *         param.
 	 * @throws ValueOutOfBoundsException
 	 */
+	@SuppressWarnings("unchecked")
 	public static Class getClassFromSize(int size) throws ValueOutOfBoundsException {
 		if (size <= 0) {
 			throw new ValueOutOfBoundsException("size lower than zero.");
 		}
-		if(size > Long.MAX_VALUE) {
+		if (size > Long.MAX_VALUE) {
 			throw new ValueOutOfBoundsException("size higher than " + Long.MAX_VALUE);
 		}
-		
+
 		if (size <= 8) {
 			return Byte.class;
 		} else if (size <= 16) {
@@ -102,17 +117,16 @@ public class SubSequencesArrayIndex {
 			return Long.class;
 		} else {
 			throw new ValueOutOfBoundsException("size is higher than a " + Long.MAX_VALUE + "? May be it's a bug.");
-		}		
-		
-		
+		}
 	}
-	
+
 	/**
 	 * @param subSymbolList
 	 * @param info
 	 */
-	private void addSubSequence(SymbolList subSymbolList, SubSequenceInfo info) { }
-	
+	private void addSubSequence(SymbolList subSymbolList, SubSequenceInfo info) {
+	}
+
 	/**
 	 * @param subSymbolList
 	 * @return
@@ -120,11 +134,88 @@ public class SubSequencesArrayIndex {
 	List<SubSequenceInfo> retrievePosition(SymbolList subSymbolList) {
 		return null;
 	}
+
+	/**
+	 * Encode a SubSequence into a integer of the type of integerClass TODO:
+	 * Remove it from a inner class
+	 * 
+	 * @author Usuario
+	 * 
+	 */
 	
+	static Hashtable<Symbol, Byte> DNASymbolToBitsSubstitionTable;
+	static {
+		DNASymbolToBitsSubstitionTable = new Hashtable<Symbol, Byte>();
+		DNASymbolToBitsSubstitionTable.put(DNATools.a(), (byte) 0x00);
+		DNASymbolToBitsSubstitionTable.put(DNATools.c(), (byte) 0x01);
+		DNASymbolToBitsSubstitionTable.put(DNATools.g(), (byte) 0x02);
+		DNASymbolToBitsSubstitionTable.put(DNATools.t(), (byte) 0x03);
+	}
+
+	protected byte getBitsFromSymbol(Symbol symbol) {
+		return DNASymbolToBitsSubstitionTable.get(symbol).byteValue();
+	}
+
+	/**
+	 * Encode a subsequence to your int representation
+	 * @param symbolList
+	 * @return a int containing the representation of the subsequence
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public int encodeToInt(SymbolList symbolList) throws InstantiationException, IllegalAccessException {
+		assert (symbolList.length() == subSequenceLength);
+		int encoded = 0;
+		for (int i = 1; i <= symbolList.length(); i++) {
+			encoded |= (getBitsFromSymbol(symbolList.symbolAt(i)) << ((symbolList.length()-i) * bitsByAlphabetSize));
+		}
+
+		return encoded;
+	}
+	
+	static Hashtable<Byte, Character> DNABitsToSymbolSubstitionTable;
+	static {
+		DNABitsToSymbolSubstitionTable = new Hashtable<Byte, Character>();
+		DNABitsToSymbolSubstitionTable.put((byte) 0x00, 'A');
+		DNABitsToSymbolSubstitionTable.put((byte) 0x01, 'C');
+		DNABitsToSymbolSubstitionTable.put((byte) 0x02, 'G');
+		DNABitsToSymbolSubstitionTable.put((byte) 0x03, 'T');
+	}
+	
+	protected char getSymbolFromBits(byte bits) {
+		return DNABitsToSymbolSubstitionTable.get(bits);
+	}
+	
+	/**
+	 * @param encoded
+	 * @return a array with the symbols that are represented in that encoded value
+	 */
+	public String decodeToString(int encoded) {
+		StringBuilder sb = new StringBuilder(subSequenceLength);
+		for (int i = subSequenceLength-1; i >= 0; i--) {
+			int shift = i * bitsByAlphabetSize;
+			int mask = ((1 << bitsByAlphabetSize) -1) << shift;
+			int value = encoded & mask;
+			byte symbolValue = (byte) (value >> shift);
+			sb.append(getSymbolFromBits(symbolValue));
+		}
+		
+		return sb.toString();				
+	}
+	
+	public SymbolList decodeToSymbolList(int encoded) throws IllegalSymbolException, BioException {
+		String sequenceString = decodeToString(encoded);		
+		return LightweightSymbolList.constructLightweightSymbolList(alphabet, alphabet.getTokenization("token"), sequenceString);
+	}
+
+	/**
+	 * Each bucket containing the positions of each sub-sequence indexed 
+	 * @author albrecht
+	 */
 	public class IndexBucket {
 		int value;
 		List<SubSequenceInfo> elements;
-		
+
 		/**
 		 * @param value
 		 */
@@ -132,14 +223,14 @@ public class SubSequencesArrayIndex {
 			this.value = value;
 			this.elements = new LinkedList<SubSequenceInfo>();
 		}
-		
+
 		/**
 		 * @return the value associate with this bucket
 		 */
 		public int getValue() {
 			return value;
 		}
-		
+
 		/**
 		 * @return the elements in this bucket
 		 */
--- src/bio/pih/tests/index/SubSequencesArrayIndexTest.java
+++ src/bio/pih/tests/index/SubSequencesArrayIndexTest.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 
 import junit.framework.TestCase;
 
+import org.biojava.bio.BioException;
+import org.biojava.bio.seq.DNATools;
+import org.biojava.bio.seq.impl.SimpleSequence;
+import org.biojava.bio.symbol.IllegalSymbolException;
+import org.biojava.bio.symbol.Symbol;
+import org.biojava.bio.symbol.SymbolList;
 import org.junit.Test;
 
 import bio.pih.index.SubSequencesArrayIndex;
 import bio.pih.index.ValueOutOfBoundsException;
+import bio.pih.seq.LightweightSymbolList;
 import bio.pih.util.SymbolListWindowIteratorFactory;
 
 /**
@@ -21,11 +28,12 @@ public class SubSequencesArrayIndexTest extends TestCase {
 
 	/**
 	 * Test if the bits value from the alphabet size is correctly calculate.
-	 * @throws ValueOutOfBoundsException 
+	 * 
+	 * @throws ValueOutOfBoundsException
 	 * 
 	 * @throws IOException
 	 * @throws ClassNotFoundException
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
 	@Test
 	public void testGetBitsBySize() throws ValueOutOfBoundsException {
@@ -42,13 +50,13 @@ public class SubSequencesArrayIndexTest extends TestCase {
 		assertEquals(6, SubSequencesArrayIndex.bitsByAlphabetSize(33));
 		assertEquals(6, SubSequencesArrayIndex.bitsByAlphabetSize(64));
 		assertEquals(7, SubSequencesArrayIndex.bitsByAlphabetSize(66));
-		assertEquals(7, SubSequencesArrayIndex.bitsByAlphabetSize(100));		
+		assertEquals(7, SubSequencesArrayIndex.bitsByAlphabetSize(100));
 		assertEquals(7, SubSequencesArrayIndex.bitsByAlphabetSize(128));
 		assertEquals(8, SubSequencesArrayIndex.bitsByAlphabetSize(200));
 		assertEquals(8, SubSequencesArrayIndex.bitsByAlphabetSize(255));
 		assertEquals(8, SubSequencesArrayIndex.bitsByAlphabetSize(256));
-	}	
-	
+	}
+
 	/**
 	 * @throws ValueOutOfBoundsException
 	 */
@@ -65,13 +73,140 @@ public class SubSequencesArrayIndexTest extends TestCase {
 		assertEquals(Integer.class, SubSequencesArrayIndex.getClassFromSize(32));
 		assertEquals(Long.class, SubSequencesArrayIndex.getClassFromSize(33));
 		assertEquals(Long.class, SubSequencesArrayIndex.getClassFromSize(63));
-		assertEquals(Long.class, SubSequencesArrayIndex.getClassFromSize(64));		
+		assertEquals(Long.class, SubSequencesArrayIndex.getClassFromSize(64));
 	}
-	
+
+	/**
+	 * @throws ValueOutOfBoundsException
+	 * @throws IllegalSymbolException
+	 */
 	@Test
-	public void testCreateSubSequenceArrayIndex() throws ValueOutOfBoundsException {
+	public void testCreateSubSequenceArrayIndex() throws ValueOutOfBoundsException, IllegalSymbolException {
 		SymbolListWindowIteratorFactory symbolListWindowIteratorFactory = SymbolListWindowIteratorFactory.getNotOverlappedFactory();
-		SubSequencesArrayIndex index = new SubSequencesArrayIndex(8, 4, symbolListWindowIteratorFactory);
-		
+		SubSequencesArrayIndex index = new SubSequencesArrayIndex(8, DNATools.getDNA(), symbolListWindowIteratorFactory);
+
+		String stringSequence = "CATGACTGGCATCAGTGCATGCATGCAGTCAGTATATATGACGC";
+		SymbolList symbolList = LightweightSymbolList.createDNA(stringSequence);
+		SimpleSequence ss = new SimpleSequence(symbolList, null, "Simple Sequence", null);
+		index.addSequence(ss);
+
+	}
+
+	/**
+	 * @throws ValueOutOfBoundsException
+	 * @throws IllegalSymbolException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	@Test
+	public void testEncodeSubSequence() throws ValueOutOfBoundsException, IllegalSymbolException, InstantiationException, IllegalAccessException {
+		SymbolListWindowIteratorFactory symbolListWindowIteratorFactory = SymbolListWindowIteratorFactory.getNotOverlappedFactory();
+		SubSequencesArrayIndex index = new SubSequencesArrayIndex(8, DNATools.getDNA(), symbolListWindowIteratorFactory);
+
+		String stringSequence = "TCGGACTG"; // 1101101000011110
+		SymbolList symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("1101101000011110", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "AACAACAA"; // 0000010000010000
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("0000010000010000", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "CCCCCCCC"; // 0101010101010101
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("0101010101010101", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "TTTTTTTT"; // 1111111111111111
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("1111111111111111", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "ACTGGTCA"; // 0001111010110100
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("0001111010110100", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "ATTTTTTT"; // 001111111111111
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("0011111111111111", 2), index.encodeToInt(symbolList));
+
+		stringSequence = "TCTAGCCA"; // 1101110010010100
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		assertEquals(Integer.parseInt("1101110010010100", 2), index.encodeToInt(symbolList));
+	}
+
+	@Test
+	public void testDecodeToStringSubSequence() throws ValueOutOfBoundsException, IllegalSymbolException, InstantiationException, IllegalAccessException {
+		SymbolListWindowIteratorFactory symbolListWindowIteratorFactory = SymbolListWindowIteratorFactory.getNotOverlappedFactory();
+		SubSequencesArrayIndex index = new SubSequencesArrayIndex(8, DNATools.getDNA(), symbolListWindowIteratorFactory);
+
+		// String stringSequence = "TCGGACTG"; // 1101101000011110
+		String stringSequence = index.decodeToString(Integer.parseInt("1101101000011110", 2));
+		assertEquals("TCGGACTG", stringSequence);
+
+		// String stringSequence = "AACAACAA"; // 0000010000010000
+		stringSequence = index.decodeToString(Integer.parseInt("0000010000010000", 2));
+		assertEquals("AACAACAA", stringSequence);
+
+		// stringSequence = "CCCCCCCC"; // 0101010101010101
+		stringSequence = index.decodeToString(Integer.parseInt("0101010101010101", 2));
+		assertEquals("CCCCCCCC", stringSequence);
+
+		// stringSequence = "TTTTTTTT"; // 1111111111111111
+		stringSequence = index.decodeToString(Integer.parseInt("1111111111111111", 2));
+		assertEquals("TTTTTTTT", stringSequence);
+
+		// stringSequence = "ACTGGTCA"; // 0001111010110100
+		stringSequence = index.decodeToString(Integer.parseInt("0001111010110100", 2));
+		assertEquals("ACTGGTCA", stringSequence);
+
+		// stringSequence = "ATTTTTTT"; // 0011111111111111
+		stringSequence = index.decodeToString(Integer.parseInt("0011111111111111", 2));
+		assertEquals("ATTTTTTT", stringSequence);
+
+		// stringSequence = "TCTAGCCA"; // 1101110010010100
+		stringSequence = index.decodeToString(Integer.parseInt("1101110010010100", 2));
+		assertEquals("TCTAGCCA", stringSequence);
+
+		// LightweightSymbolList.createLightSymbolList(DNATools.getDNA(),
+		// symbols);
+	}
+
+	@Test
+	public void testDecodeToSequenceSubSequence() throws ValueOutOfBoundsException, InstantiationException, IllegalAccessException, BioException {
+		SymbolListWindowIteratorFactory symbolListWindowIteratorFactory = SymbolListWindowIteratorFactory.getNotOverlappedFactory();
+		SubSequencesArrayIndex index = new SubSequencesArrayIndex(8, DNATools.getDNA(), symbolListWindowIteratorFactory);
+
+		String stringSequence = "TCGGACTG"; // 1101101000011110
+		SymbolList symbolList = LightweightSymbolList.createDNA(stringSequence);
+		int encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "AACAACAA"; // 0000010000010000
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "CCCCCCCC"; // 0101010101010101
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "TTTTTTTT"; // 1111111111111111
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "ACTGGTCA"; // 0001111010110100
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "ATTTTTTT"; // 001111111111111
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
+
+		stringSequence = "TCTAGCCA"; // 1101110010010100
+		symbolList = LightweightSymbolList.createDNA(stringSequence);
+		encoded = index.encodeToInt(symbolList);
+		assertEquals(symbolList, index.decodeToSymbolList(encoded));
 	}
 }
