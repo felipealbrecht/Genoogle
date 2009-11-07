bebc7471021fde965da5e254412fe090fff3d808
albrecht <albrecht@c56685f5-d0d1-4d63-ba2d-6bec8b09bb16>
2009-11-07 10:02:08 +0000

    Start scripts using JAVA_HOME variable

--- run_gen.sh
+++ run_gen.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-/home/albrecht/java/jrmc-3.0.3-1.6.0/bin/java \
+$JAVA_HOME/bin/java \
  -Xms1024m \
  -Xmx2048m \
  -server \
- -XXaggressive:opt \
- -XXtlaSize:min=4096k \
- -Dcom.sun.management.jmxremote \
  -classpath genoogle.jar:lib/* \
  bio.pih.SOIS -g
-
-
-
-
-
