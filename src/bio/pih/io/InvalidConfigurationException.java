c1eddbaa827d5a9868a261fa5c613b4ce4254fd5
albrecht <albrecht@c56685f5-d0d1-4d63-ba2d-6bec8b09bb16>
2009-11-18 04:03:10 +0000

    Removed 'storage' field at fasta files configuration.

--- src/bio/pih/io/InvalidConfigurationException.java
+++ src/bio/pih/io/InvalidConfigurationException.java
@@ -2,6 +2,8 @@ package bio.pih.io;
 
 public class InvalidConfigurationException extends Exception {
 
+	private static final long serialVersionUID = -7849602904181806080L;
+
 	public InvalidConfigurationException(String message) {
 		super(message);
 	}
